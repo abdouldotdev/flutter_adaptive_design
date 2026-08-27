095fa9fc46975d504be29f6ae21bd836e7ee2876
Abdoul <prodevking1@gmail.com>
2026-08-27 19:06:04 +0000

    fix(lint): remove unused imports in test files

--- test/widgets/chips_test.dart
+++ test/widgets/chips_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_design/flutter_adaptive_design.dart';
 import 'package:flutter_adaptive_design/testing.dart';
